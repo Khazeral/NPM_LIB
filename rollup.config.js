a94b5253327c195ad42d7e04c09b030376037270
Khazeral <wandehousse@gmail.com>
2023-05-13 12:44:13 +0200

    roijgr

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import typescript from "@rollup/plugin-typescript";
 
 export default [
   {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: [
       {
         file: "dist/index.js",
