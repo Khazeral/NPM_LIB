1f34484a5f8af38adb295dc2ec3b916482a82eed
Khazeral <wandehousse@gmail.com>
2023-05-13 12:15:40 +0200

    lol

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import { dir } from "console";
+
+export default [
+  {
+    input: "./src/index.js",
+    output: {
+      dir: "dist",
+      format: "umd",
+      name: "modal",
+      exports: "named",
+    },
+    plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+      }),
+      external(),
+      resolve(),
+      terser(),
+    ],
+  },
+];
