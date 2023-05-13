c126056e3cbbaa972f611e5b84a1a50376509996
Khazeral <wandehousse@gmail.com>
2023-05-13 03:24:07 +0200

    lol

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 
 export default [
   {
