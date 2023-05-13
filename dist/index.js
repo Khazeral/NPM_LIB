1541fe1debc7fab2aa494a3a3d8ffc3584d8ba06
Khazeral <wandehousse@gmail.com>
2023-05-13 05:14:39 +0200

    lol

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("../../../../../../../../components/Modal");Object.defineProperty(exports,"Modal",{enumerable:!0,get:function(){return e.Modal}});
