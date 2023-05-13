6d0871b4a0d43a0a837d8d117e02e8129dac2f16
Khazeral <wandehousse@gmail.com>
2023-05-13 12:18:05 +0200

    vepvierb

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,22 @@ import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
-import { dir } from "console";
 
 export default [
   {
     input: "./src/index.js",
-    output: {
-      dir: "dist",
-      format: "umd",
-      name: "modal",
-      exports: "named",
-    },
+    output: [
+      {
+        file: "./dist/index.js",
+        format: "cjs",
+        export: "auto",
+      },
+      {
+        file: "dist/index.es.js",
+        format: "es",
+        exports: "named",
+      },
+    ],
     plugins: [
       postcss({
         plugins: [],
