169c3a468d7f2b22be772cfd8275eaf7c7e0d86e
Khazeral <wandehousse@gmail.com>
2023-05-13 03:55:02 +0200

    ik

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -8,7 +8,7 @@ type ModalProps = {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export function Modal({ title, description, displayed, onClick }: ModalProps) {
+export const Modal = ({ title, description, displayed, onClick }: ModalProps) => {
   return (
     <div className="full-screen" data-display={displayed}>
       <div className="modale">
