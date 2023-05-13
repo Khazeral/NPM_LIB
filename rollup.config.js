d5731c58a8b062b1a71dfb853e5861a372e5b4c2
Khazeral <wandehousse@gmail.com>
2023-05-13 12:20:05 +0200

    f

--- rollup.config.js
+++ rollup.config.js
@@ -6,10 +6,10 @@ import { terser } from "rollup-plugin-terser";
 
 export default [
   {
-    input: "./src/index.js",
+    input: "src/index.js",
     output: [
       {
-        file: "./dist/index.js",
+        file: "dist/index.js",
         format: "cjs",
         export: "auto",
       },
